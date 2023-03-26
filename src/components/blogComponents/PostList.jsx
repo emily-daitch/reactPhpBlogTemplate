2cea68e4100defd64d7a77e3db45a6475c5f7f0b
Emily <emily.daitch@gmail.com>
2023-03-26 19:11:55 -0400

    update postlist theme

--- src/components/blogComponents/PostList.jsx
+++ src/components/blogComponents/PostList.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Box, Badge } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
 
-export default function PostList({id, title, content, image}) {
-
+export default function PostList({id, title, content, image, theme}) {
     function slug(string) {
         return string.toLowerCase()
         .replace(/ /g, '-')
@@ -18,6 +17,7 @@ export default function PostList({id, title, content, image}) {
                     <Box slug={title}
                         mt='1'
                         fontWeight='semibold'
+                        color={theme === 'light' ? '#333' : '#fff'}
                         as='h4'
                         lineWeight='tight'>
                         {title}
