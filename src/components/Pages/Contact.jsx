a63233f5f7692df1eb1a7efe2284d01137547891
Emily <emily.daitch@gmail.com>
2023-03-25 20:50:18 -0400

    fix contact link

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Contact() {
     return (
         <div>Contact
-        <br/><Link to='linkedin.com/in/emilydaitch'>Linkedin</Link>
+        <br/><Link to='https://www.linkedin.com/in/emilydaitch'>Linkedin</Link>
         <br/><button onClick={() => window.location = 'mailto:emily.daitch@gmail.com'}>Email Me</button>
         </div>
     )
