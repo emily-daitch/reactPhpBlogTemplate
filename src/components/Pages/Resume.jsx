a2db5216c41373e3efa740b4ed29b90f62df6eab
Emily Daitch <95369494+emily-daitch@users.noreply.github.com>
2023-03-28 16:42:13 -0400

    Update Resume.jsx
    clean Resume

--- src/components/Pages/Resume.jsx
+++ src/components/Pages/Resume.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Resume({theme}) {
-    const color = theme === 'light' ? {color:'#333'} : {color:'#fff'};
+    const isLightTheme = theme === 'light';
+    const styleColor = isLightTheme ? {color:'#333'} : {color:'#fff'};
+    const color = isLightTheme ? '#333' : '#fff';
     return (
-        <div style={color}><p>Resume</p>
-        <br/><Link color={theme === 'light' ? '#333' : '#fff'} to='https://www.emilydaitch.click/DaitchResume2023.pdf'>
-            <p color={theme === 'light' ? '#333' : '#fff'}>View/Download my Resume</p>
+        <div style={styleColor}><p>Resume</p>
+            <br/><Link color={color} to='https://www.emilydaitch.click/DaitchResume2023.pdf'>
+                <p color={color}>View/Download my Resume</p>
             </Link>
         </div>
     )
