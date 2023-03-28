c544cf57ef596c332fc93f771ccdfee2b6f76e90
Emily Daitch <95369494+emily-daitch@users.noreply.github.com>
2023-03-28 16:39:16 -0400

    Update Contact.jsx
    format Contact

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -5,8 +5,8 @@ export default function Contact({theme}) {
     const color = theme === 'light' ? {color:'#333'} : {color:'#fff'};
     return (
         <div style={color}><p>Contact</p>
-        <br/><Link to='https://www.linkedin.com/in/emilydaitch'>Linkedin</Link>
-        <br/><button onClick={() => window.location = 'mailto:emily.daitch@gmail.com'}>Email Me</button>
+            <br/><Link to='https://www.linkedin.com/in/emilydaitch'>Linkedin</Link>
+            <br/><button onClick={() => window.location = 'mailto:emily.daitch@gmail.com'}>Email Me</button>
         </div>
     )
 }
