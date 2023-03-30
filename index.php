ad4a856677e8b0e2041b0a2d3d0035b18cc6bcc1
Emily <emily.daitch@gmail.com>
2023-03-30 10:15:01 -0400

    missed index update

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
+# This file can be used for testing by removing 
+# RewriteRule ^ index.html [QSA,L]
+# from your root level .htaccess file
 error_reporting(E_ALL);
 ini_set('display_error', '1');
 
 require('api/services/DB.php');
 use Services\DB;
 
-require('api/controllers/PostsController.php');
-//use Api\Controllers\PostsController;
-
-$dir    = 'api/services';
-$dir2    = 'api/controllers';
-$files1 = scandir($dir);
-$files2 = scandir($dir2);
-
-print_r($files1);
 echo "Hello";
-print_r($files2);
