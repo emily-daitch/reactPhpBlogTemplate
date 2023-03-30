c3975804f330a592c1ca7b1e03bf9d5c6b4a2795
Emily <emily.daitch@gmail.com>
2023-03-30 10:15:16 -0400

    update DB.php

--- api/services/DB.php
+++ api/services/DB.php
@@ -1,27 +1,16 @@
 <?php
 
 namespace services;
-include '../../../sharkweek.php'; // Lives outside repo / webroot
 
+include 'sharkweek-staging.php'; // Lives outside repo / webroot
 use mysqli;
 
 class DB
 {
-    // For local development
-    //     public $servername = 'localhost';
-    //     public $username = 'root';
-    //     public $password = '';
-    //     public $database = 'react_php';
-    
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    unset($servername, $username, $password, $database);
-
     public function database()
     {
+        global $servername, $username, $password, $database;
         // Making connection
-        // local development:
-            //$conn = new mysqli($this->servername, $this->username, $this->password, $this->database);
         $conn = mysqli_connect($servername, $username, $password, $database);
         unset($servername, $username, $password, $database);
         
