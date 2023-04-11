b2de517b08fb7810d26a8c0d5d08b8448a525061
Emily <emily.daitch@gmail.com>
2023-04-11 15:24:10 -0400

    add UsersController

---
+++ api/controllers/UsersController.php
@@ -0,0 +1,68 @@
+<?php
+namespace Api\Controllers;
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+define('__ROOT__', dirname(dirname(dirname(__FILE__))));
+require_once(__ROOT__.'/api/services/DB.php');
+use Services\DB;
+class UsersController
+{
+    public $conn = null;
+
+    public function __construct()
+    {
+        // Create connection
+        $this->conn = (new DB())->database();
+    }
+
+    // Uncomment for local development to resolve CORS issue
+    public function getHeaders()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Credentials: *");
+        header('Access-Control-Max-Age: 86400');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        }
+    }
+
+    public function submitNewUser()
+    {
+        try
+        {
+            $this->getHeaders();
+            $submitResponse = null;
+            //$id = $_GET['id'] ?? null;
+            $email = 'email';
+            $password = 'password';
+
+            if($username)
+            {
+                // for now, make username the email -- we will probably remove username as I don't plan to use one
+                // this will also give us the chance to write a DB script to bulk edit the table ^.^
+                // don't set id, let it suto-increment
+                $sql = "INSERT INTO users ('username', 'email', 'password') VALUES ($email, $email, $password)";
+
+                // use argon2id to salt PW
+                // pepper PW ?
+
+                $submitResponse = mysqli_query($this->conn, $sql);
+            }
+
+            echo json_encode($submitResponse, JSON_PRETTY_PRINT);
+        }
+        catch(\Exception $e)
+        {
+            var_dump($e->getMessage);
+            exit;
+        }
+    }
+}
