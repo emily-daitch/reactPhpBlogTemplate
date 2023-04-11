6de12c28f219704be4e84b5dc09c9faff42a4a23
Emily <emily.daitch@gmail.com>
2023-04-11 14:57:54 -0400

    edit registration page - add forms

--- src/components/Pages/Register.tsx
+++ src/components/Pages/Register.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormControl, Input } from '@chakra-ui/react';
 
 type Props = {
     theme: string
@@ -8,8 +9,27 @@ export default function Register({theme}: Props) {
     const isLightTheme = theme === 'light';
     const styleColor = isLightTheme ? {color:'#333'} : {color:'#fff'};
     const color = isLightTheme ? '#333' : '#fff';
-    
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const initialRef = React.useRef(null);
+
     return(
-        <h2>Register</h2>
+        <>
+            <h2>Register</h2>
+            <FormControl mt={4}>
+                <Input placeholder='Email'
+                    ref={initialRef}
+                    color={color}
+                    onChange={(e)=> setEmail(e.target.value)}
+                />
+            </FormControl>
+            <FormControl mt={4}>
+                <Input placeholder='Password'
+                    ref={initialRef}
+                    color={color}
+                    onChange={(e)=> setPassword(e.target.value)}
+                />
+            </FormControl>
+        </>
     );
 }
